f5b856a55e50901cd13b7aa34270dbe23a683eed
Mr. User <mr@email.com>
2016-02-29 23:43:38 +0100

    STY: reduced indent of main()

--- vimPyTest.py
+++ vimPyTest.py
@@ -2,13 +2,16 @@
 # @TODO NerdTree
 # @TODO SuperTab or Python Mode
 import pudb; pudb.set_trace()  # XXX BREAKPOINT
+import numpy as np
 
 def main():
-        print("neat")
-        x = 1
-        for i in range(1, 100):
-            print ('x={}'.format(x))
-            x += 1
+    print("neat")
+    x = 1
+    for i in range(1, 100):
+        print ('x={}'.format(x))
+        x += 1
+    np.arange(10).reshape(2,5)
+    numpy.logspace
 
 if __name__ == "__main__":
     print('Starting script...\n')
